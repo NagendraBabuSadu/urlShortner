76a667e51a11b6605f41592b24a00e5151579e25
NagendraBabuSadu <89573050+NagendraBabuSadu@users.noreply.github.com>
2025-04-27 16:06:33 +0530

    changed CORS

--- week2/urlShortner/server-app/src/server.ts
+++ week2/urlShortner/server-app/src/server.ts
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDb from "./config/dbconfig";
-import shortUrl from './routes/shortUrl'
+import shortUrl from "./routes/shortUrl";
 dotenv.config();
 connectDb();
 
@@ -10,30 +10,33 @@ const port = process.env.PORT || 5001;
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(cors({
-    credentials: true,
-    origin: "http://localhost:3000"
-}))
+app.use(express.urlencoded({ extended: true }));
+// app.use(
+//   cors({
+//     credentials: true,
+//     origin: "http://localhost:3000",
+//   })
+// );
 
 const allowedOrigins = [
   "https://urlshortner-hj53.onrender.com",
-  ];
-  
-  app.use(
-    cors({
-      origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-          callback(null, true);
-        } else {
-          callback(new Error("Not allowed by CORS"));
-        }
-      },
-    })
-  );
+  "http://localhost:3000",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
 
 app.use("/api", shortUrl);
 
 app.listen(port, () => {
-    console.log(`Server started on port: ${port}` )
+  console.log(`Server started on port: ${port}`);
 });
